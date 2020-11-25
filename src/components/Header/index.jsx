f1f2b7ddf502eae71b3aaf55edd47ed58d2f7aeb
Alllex202 <Alllex.202@gmail.com>
2020-11-25 17:09:55 +0500

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -280,6 +280,7 @@ const useStyles = makeStyles({
   notification: {
     display: 'flex',
     width: 267,
+    height: 'auto',
     justifyContent: 'flex-start',
     // marginBottom: 12,
     padding: '6px 18px 4px 18px',
